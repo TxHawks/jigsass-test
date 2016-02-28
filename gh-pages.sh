fdae7d6d6cdc9fd99c4a9471bf26638374a1a858
TxHawks <tofu.hawks@gmail.com>
2016-02-28 05:24:12 +0200

    Add satatus messages to gh-pages script

--- gh-pages.sh
+++ gh-pages.sh
@@ -4,9 +4,11 @@
 
 # Dependcies: `awk` and `git`, must be run on a Unix system.
 
+echo 'Updating gh-pages'
 cd "$(dirname $0)";
 parent_sha=$(git show-ref -s refs/heads/gh-pages)
 doc_sha=$(git ls-tree -d HEAD gh-pages | awk '{print $3}')
 new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p $parent_sha)
 git update-ref refs/heads/gh-pages $new_commit
+echo 'done'
 exit;
